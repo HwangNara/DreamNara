79cfb14c1719975265d5e93be29a7d6f80fc9e70
COUNTRY\country <hnaras@naver.com>
2017-07-06 00:36:29 +0900

    main 이미지 변경 (사이즈, 흑백, 위아래 여백) max-width : 1024px

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
-$(function(){
+var page = {};
+page.sectionNumber = 4;
 
-	//Check to see if the window is top if not then display button
-	$(window).scroll(function(){
+
+$(function() {
+    //Check to see if the window is top if not then display button
+	$(window).scroll(function() {
 		if ($(this).scrollTop() > 50) {
 			$('.go-top').fadeIn();
 		} else {
@@ -9,24 +12,62 @@ $(function(){
 		}
 	});
 
-    $(window).scroll(function(){
+    $(window).scroll(function() {
         if ($(this).scrollTop() > 40) {
             $('.nav-tab').fadeIn();
-        } else {
+        } /*else {
             $('.nav-tab').fadeOut();
-        }
+        }*/
     });
 
-	//Click event to scroll to top
-	$('.go-top').click(function(){
+	// click event to scroll to top
+	$('.go-top').click(function() {
 		$('html, body').animate({scrollTop : 0}, 600);
 		return false;
 	});
 
+	// tab link move
+    page.move = function(tag) {
+        window.location.href = tag;
+    }
 
-
-
-
-
-	
+    // indicator scrollspy
+    $(window).scroll(function() {
+        page.currentTop = $(document).scrollTop();
+        if ($('#test2').offset().top > page.currentTop) {
+            page.moveIndicator(0);
+        } else if ($('#test3').offset().top > page.currentTop) {
+            page.moveIndicator(1);
+        } else if ($('#test4').offset().top > page.currentTop) {
+            page.moveIndicator(2);
+        } else {
+            page.moveIndicator(3);
+        }
+    });
 });
+
+page.moveIndicator = function (number) {
+    page.category = ["WELCOME", "GREETING", "GALLERY", "DIRECTIONS"];
+    console.log(page.category[number]);
+
+    page.browserWidth = $(window).width();
+    page.onePieceWidth = page.browserWidth / page.sectionNumber;
+    page.scrollSpyLeft = page.getScrollSpyLeftValue(page.onePieceWidth, number);
+    page.scrollSpyRight = page.getScrollSpyRightValue(page.browserWidth, page.onePieceWidth, page.scrollSpyLeft);
+    page.modifyIndicatorCss(page.scrollSpyLeft, page.scrollSpyRight);
+};
+
+page.getScrollSpyLeftValue = function (onePieceWidth, order) {
+    return order * onePieceWidth;
+};
+
+page.getScrollSpyRightValue = function (browserWidth, onePieceWidth, leftValue) {
+    return browserWidth - onePieceWidth - leftValue;
+};
+
+page.modifyIndicatorCss = function (scrollSpyLeft, scrollSpyRight) {
+    $('.indicator').css({
+        'left':  scrollSpyLeft + 'px',
+        'right': scrollSpyRight + 'px'
+    });
+};
