6de5065207e7f412676d5adbe9116eb643dbb962
COUNTRY\country <hnaras@naver.com>
2017-08-04 22:52:02 +0900

    add map controller & refactoring

--- js/daumMap.js
+++ js/daumMap.js
@@ -1,36 +1,34 @@
-var map = {};
-map.showLocation = function () {
-    var mapContainer = document.getElementById('map'), // 지도를 표시할 div
-        mapOption = {
-            center: new daum.maps.LatLng(37.55744494643207, 126.93140262289675), // 지도의 중심좌표
-            level: 5 // 지도의 확대 레벨
-        };
-    var map = new daum.maps.Map(mapContainer, mapOption);
-
-    map.setZoomable(false);
-    map.setDraggable(false);
-
-    // 마커가 표시될 위치
-    var markerPosition  = new daum.maps.LatLng(37.55744494643207, 126.93140262289675);
-
-    // 마커를 생성
-    var marker = new daum.maps.Marker({
-        position: markerPosition
-    });
-
-    // 마커가 지도 위에 표시되도록 설정
-    marker.setMap(map);
-
-    var iwContent =
+var newMap = {
+    mapContainer : document.getElementById('map'),
+    mapOption : {
+        center: new daum.maps.LatLng(37.55744494643207, 126.93140262289675), // 신촌교회
+        level: 5 // 지도의 확대 레벨
+    },
+    infoWindowContent :
         '<div class="map-text-div" style="padding:5px;">신촌장로교회 <br>' +
-        '   <strong><a href="http://map.daum.net/link/to/9188779" target="_blank">길찾기</a></strong>' +
+            '   <strong><a href="http://map.daum.net/link/to/9188779" target="_blank">길찾기</a></strong>' +
         '</div>',
-        iwPosition = new daum.maps.LatLng(37.55744494643207, 126.93140262289675); //인포윈도우 표시 위치
+    showLocation : function () {
+        var map = new daum.maps.Map(this.mapContainer, this.mapOption);
 
-    // 인포윈도우를 생성
-    var infowindow = new daum.maps.InfoWindow({
-        position : iwPosition,
-        content : iwContent
-    });
-    infowindow.open(map, marker);
+        // 줌, 드래그, 지도변환, 확대축소 설정
+        map.setZoomable(false);
+        map.setDraggable(false);
+        map.addControl(new daum.maps.MapTypeControl(), daum.maps.ControlPosition.TOPRIGHT);
+        map.addControl(new daum.maps.ZoomControl(), daum.maps.ControlPosition.RIGHT);
+
+        // 마커
+        var marker = new daum.maps.Marker({
+            position: this.mapOption.center,
+            map: map
+        });
+
+        // 인포윈도우
+        var infowindow = new daum.maps.InfoWindow({
+            position: this.mapOption.center,
+            content: this.infoWindowContent
+        });
+        infowindow.open(map, marker);
+    }
 };
+
